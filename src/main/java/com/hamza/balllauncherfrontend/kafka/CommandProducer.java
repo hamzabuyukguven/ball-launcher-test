25584972a99e1d39dd876227898e9278f704eaee
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-17 14:16:23 +0300

    Update CommandProducer.java

--- src/main/java/com/hamza/balllauncherfrontend/kafka/CommandProducer.java
+++ src/main/java/com/hamza/balllauncherfrontend/kafka/CommandProducer.java
@@ -1,4 +1,4 @@
-package com.hamza.ballauncherfrontend.kafka;
+package com.hamza.balllauncherfrontend.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
