7510f925f6d9684c34b7dc9179a8e16222d37d0f
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-17 14:37:49 +0300

    Create SystemStatus.java

---
+++ src/main/java/com/hamza/balllauncherfrontend/kafka/SystemStatus.java
@@ -0,0 +1,54 @@
+package com.hamza.balllauncherfrontend.kafka;
+
+public class SystemStatus {
+
+    private boolean connected;
+    private String availability;
+    private double platformAngle;
+    private double cannonAngle;
+    private long timeStamp;
+
+    public SystemStatus() {
+        // Jackson için boş constructor
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected(boolean connected) {
+        this.connected = connected;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public double getPlatformAngle() {
+        return platformAngle;
+    }
+
+    public void setPlatformAngle(double platformAngle) {
+        this.platformAngle = platformAngle;
+    }
+
+    public double getCannonAngle() {
+        return cannonAngle;
+    }
+
+    public void setCannonAngle(double cannonAngle) {
+        this.cannonAngle = cannonAngle;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
