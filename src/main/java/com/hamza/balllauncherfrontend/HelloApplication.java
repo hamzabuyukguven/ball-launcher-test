01c32ebb31aeffb00766c5a177559afaf445e413
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-22 14:37:33 +0300

    Update HelloApplication.java

--- src/main/java/com/hamza/balllauncherfrontend/HelloApplication.java
+++ src/main/java/com/hamza/balllauncherfrontend/HelloApplication.java
@@ -12,12 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 730, 850);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
-        Thread consumerThread = new Thread(new SystemStatusConsumer("10.152.220.16:9092", "launcher-group", message ->{
+        Thread consumerThread = new Thread(new SystemStatusConsumer("172.20.10.3:9092", "launcher-group", message ->{
             System.out.println("gelen mesaj:" +message);
         }));
         consumerThread.setDaemon(true);
