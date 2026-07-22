7227c1c853f4b39ea43cc934007eaae22560e09a
erenceri <merenceri@gmail.com>
2026-07-22 08:31:16 +0300

    Add files via upload

---
+++ untitled2/src/main/java/com/launcher/kafka/model/LauncherTelemetry.java
@@ -0,0 +1,31 @@
+package com.launcher.kafka.model;
+
+public class LauncherTelemetry {
+
+    private double targetX;
+    private double targetY;
+
+    public LauncherTelemetry() {
+    }
+
+    public LauncherTelemetry(double targetX, double targetY) {
+        this.targetX = targetX;
+        this.targetY = targetY;
+    }
+
+    public double getTargetX() {
+        return targetX;
+    }
+
+    public void setTargetX(double targetX) {
+        this.targetX = targetX;
+    }
+
+    public double getTargetY() {
+        return targetY;
+    }
+
+    public void setTargetY(double targetY) {
+        this.targetY = targetY;
+    }
+}
