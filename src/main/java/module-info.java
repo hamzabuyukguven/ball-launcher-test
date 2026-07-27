6c2b0c3778fbcc7dcd6bc482014e9590c3f29cbd
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-27 14:33:01 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,9 @@ module com.hamza.balllauncherfrontend {
     requires org.slf4j;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
-    
-    
-
+    opens com.hamza.balllauncherfrontend.kafka to com.fasterxml.jackson.databind;
 
     opens com.hamza.balllauncherfrontend to javafx.fxml;
     exports com.hamza.balllauncherfrontend;
+    exports com.hamza.balllauncherfrontend.kafka;
 }
