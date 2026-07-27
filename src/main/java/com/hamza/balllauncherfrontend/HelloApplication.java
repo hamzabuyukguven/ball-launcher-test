a7baf638691fda62722efe08d9e5fd16200569d3
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-27 14:32:20 +0300

    Update HelloApplication.java

--- src/main/java/com/hamza/balllauncherfrontend/HelloApplication.java
+++ src/main/java/com/hamza/balllauncherfrontend/HelloApplication.java
@@ -4,28 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import com.hamza.balllauncherfrontend.kafka.SystemStatusConsumer;
-import com.hamza.balllauncherfrontend.HelloController;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-@Override
-public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 730, 850);
+    private HelloController controller;
 
-    HelloController controller = fxmlLoader.getController();
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-    stage.setTitle("Ball Launcher Control Panel");
-    stage.setScene(scene);
+        Scene scene = new Scene(loader.load(), 1180, 760);
 
-    stage.setOnCloseRequest(event -> {
+        controller = loader.getController();
+
+        stage.setTitle("Ball Launcher Control Panel");
+        stage.setMinWidth(1000);
+        stage.setMinHeight(680);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
         if (controller != null) {
             controller.shutdown();
         }
-    });
+    }
 
-    stage.show();
+    public static void main(String[] args) {
+        launch();
+    }
 }
-}
+
