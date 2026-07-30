662d2e5e867901951f0f63294331fcf217840844
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-30 19:30:04 +0300

    Update HelloController.java

--- src/main/java/com/hamza/balllauncherfrontend/HelloController.java
+++ src/main/java/com/hamza/balllauncherfrontend/HelloController.java
@@ -191,7 +191,7 @@ public class HelloController {
 
     @FXML
     public void initialize() {
-        ballTypeField.getItems().addAll("A", "B", "C");
+        ballTypeField.getItems().addAll("76 MM");
         ballTypeField.getSelectionModel().selectFirst();
 
         forbiddenSectors = AppConfig.getForbiddenSectors();
