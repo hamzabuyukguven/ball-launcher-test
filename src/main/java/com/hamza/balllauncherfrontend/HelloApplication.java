2984e8f73f1f0e534b24be562505c8efb4a0cae0
hamzabuyukguven <hamzabuyukguven@gmail.com>
2026-07-30 19:30:22 +0300

    Update HelloApplication.java

--- src/main/java/com/hamza/balllauncherfrontend/HelloApplication.java
+++ src/main/java/com/hamza/balllauncherfrontend/HelloApplication.java
@@ -2,7 +2,9 @@ package com.hamza.balllauncherfrontend;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,13 +16,20 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(loader.load(), 820, 880);
+
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+        double width = Math.min(840, screen.getWidth() - 40);
+        double height = Math.min(900, screen.getHeight() - 40);
+
+        Scene scene = new Scene(loader.load(), width, height);
         controller = loader.getController();
 
         stage.setTitle("Gun Launcher Control Panel");
-        stage.setMinWidth(800);
-        stage.setMinHeight(700);
         stage.setScene(scene);
+        stage.setMinWidth(700);
+        stage.setMinHeight(500);
+        stage.setX(screen.getMinX() + (screen.getWidth() - width) / 2);
+        stage.setY(screen.getMinY() + Math.max(0, (screen.getHeight() - height) / 2));
         stage.show();
     }
 
